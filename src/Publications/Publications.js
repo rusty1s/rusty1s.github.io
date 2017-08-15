5632699831673b545fc0d468006ae78f545722d8
rusty1s <matthias.fey@tu-dortmund.de>
2017-08-15 21:06:29 +0200

    bibtex toggle

--- src/Publications/Publications.js
+++ src/Publications/Publications.js
@@ -1,51 +1,80 @@
-import React from "react";
+import React, { Component } from "react";
+import cx from "classnames";
 
 import "./publications.css";
 import Code from "../components/Code";
 import publications from "./publications.json";
 
-// TODO: Copy to clipboard
+class Publications extends Component {
+  state = {
+    openedBibtexs: []
+  };
 
-const Publications = () =>
-  <div className="publications">
-    {publications.map(p =>
-      <section key={p.name} className="publication">
-        <header className="name">
-          {p.name}
-        </header>
-        {p.author &&
-          <div className="author">
-            {p.author}
-          </div>}
-        {p.description.map(des =>
-          <div key={des} className="description">
-            {des}
-          </div>
+  toggleBibtex = id => {
+    let { openedBibtexs } = this.state;
+    if (openedBibtexs.includes(id))
+      openedBibtexs = openedBibtexs.filter(idx => idx !== id);
+    else openedBibtexs = [...openedBibtexs, id];
+
+    this.setState({ openedBibtexs });
+  };
+
+  render() {
+    return (
+      <div className="publications">
+        {publications.map((p, id) =>
+          <section key={p.name} className="publication">
+            <header className="name">
+              {p.name}
+            </header>
+            {p.author &&
+              <div className="author">
+                {p.author}
+              </div>}
+            {p.description.map(des =>
+              <div key={des} className="description">
+                {des}
+              </div>
+            )}
+            <footer>
+              [
+              {p.refs.map(ref =>
+                <span key={ref.name}>
+                  <a href={ref.href}>
+                    {ref.name}
+                  </a>
+                  {", "}
+                </span>
+              )}
+              <button
+                className="bibtex-button"
+                onClick={() => this.toggleBibtex(id)}
+              >
+                Bibtex
+              </button>
+              ]
+            </footer>
+            <Code
+              className={cx(
+                "bibtex",
+                this.state.openedBibtexs.includes(id) && "opened"
+              )}
+            >
+              <div>
+                {`@${p.bibtex.type}{${p.bibtex.id},`}
+              </div>
+              {p.bibtex.content.map(line =>
+                <div key={line}>{`  ${line},`}</div>
+              )}
+              <div>
+                {"}"}
+              </div>
+            </Code>
+          </section>
         )}
-        <footer>
-          [
-          {p.refs.map(ref =>
-            <span key={ref.name}>
-              <a href={ref.href}>
-                {ref.name}
-              </a>
-              {", "}
-            </span>
-          )}
-          <button>Bibtex</button>
-          ]
-        </footer>
-        <Code>
-          <div>
-            {`@${p.bibtex.type}{${p.bibtex.id},`}
-          </div>
-          {p.bibtex.content.map(line => <div key={line}>{`  ${line},`}</div>)}
-          <div>
-            {"}"}
-          </div>
-        </Code>
-      </section>
-    )}
-  </div>;
+      </div>
+    );
+  }
+}
 
 export default Publications;
