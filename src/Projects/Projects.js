66fd5149677caa2ec0649f1339977ded74573d4b
rusty1s <matthias.fey@tu-dortmund.de>
2017-08-28 18:53:08 +0200

    added projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const Projects = () => <div />;
 
 export default Projects;
+
+// dotfiles
+// embedded_gcnn
+// table2excel
+// js-dev-utils
+// deep-learning-cheatsheet
+// mongoose-18n-localize
+// react-documentviewer
+// RSClipperWrapper
