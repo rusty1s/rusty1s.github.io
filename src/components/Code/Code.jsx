bb056cea65f2717b6fbd061def9adc48dfe6638c
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-02 18:58:22 +0200

    bugfixes

--- src/components/Code/Code.jsx
+++ src/components/Code/Code.jsx
@@ -1,11 +1,8 @@
-import React, { Children } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import copy from 'copy-to-clipboard';
 
 import styles from './code.css';
-
-import IconLink from '../IconLink';
 
 const propTypes = {
   children: PropTypes.node,
@@ -17,19 +14,9 @@ const defaultProps = {
   className: null,
 };
 
-const getText = children =>
-  Children.map(children, child => child.props.children).join('\n');
-
 const Code = ({ children, className, ...props }) => (
   <pre className={cx(className, styles.main)} {...props}>
     {children}
-    <IconLink
-      className={styles.clipboard}
-      containerComponent="button"
-      onClick={() => copy(getText(children))}
-      icon="clipboard"
-      title="Copy to clipboard"
-    />
   </pre>
 );
 
