27124ac59880be8eeedf1597b42d2949d2dae6fe
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-04 00:12:08 +0100

    added supervised students section

---
+++ src/Teaching/Student.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './student.css';
+
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  isMaster: PropTypes.bool.isRequired,
+};
+
+const Student = props => (
+  <div className={styles.main}>
+    <div className={styles.name}>{props.name}</div>
+    <div>{props.author}</div>
+    <div className={styles.description}>
+      {props.isMaster ? "Master's" : "Bachelor's"} thesis ({props.year})
+    </div>
+  </div>
+);
+
+Student.propTypes = propTypes;
+
+export default Student;
+
--- src/Teaching/Teaching.jsx
+++ src/Teaching/Teaching.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 
 import styles from './teaching.css';
 import lectures from './lectures.json';
+import students from './students.json';
 
 import Title from '../components/Title';
 import Lecture from './Lecture';
+import Student from './Student';
 
 const Teaching = () => (
   <div className={styles.main}>
     <Title>Lectures</Title>
     {lectures.map(lecture => (
       <Lecture key={`${lecture.name}-${lecture.year}`} {...lecture} />
+    ))}
+
+    <Title>Supervised Students</Title>
+    {students.map(student => (
+      <Student key={`${student.name}-${student.year}`} {...student} />
     ))}
   </div>
 );
